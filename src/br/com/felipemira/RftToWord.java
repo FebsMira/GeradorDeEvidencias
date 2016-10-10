84c8783cd6bc580693c6e2cea0e603ab21f01617
Felipe Mira <felipe.mira@fatec.sp.gov.br>
2016-10-10 00:27:30 -0300

    Caminho template ajustado.

--- src/br/com/felipemira/RftToWord.java
+++ src/br/com/felipemira/RftToWord.java
@@ -8,13 +8,13 @@ public class RftToWord {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		WordAutomacao word = new WordAutomacao("Evidencia"
-				, "C:\\Users\\Felipe Mira\\Documents\\Inmetrics\\Evidencias\\"
-				, "C:\\Users\\Felipe Mira\\Documents\\Inmetrics\\Template\\Template.docx"
-				, "C:\\Users\\Felipe Mira\\Documents\\Inmetrics\\Template\\evidencia.png");
+				, "C:\\Users\\Felipe Mira\\Documents\\WordToPDF\\Evidencias\\"
+				, "C:\\Users\\Felipe Mira\\Documents\\WordToPDF\\Template\\Template.docx"
+				, "C:\\Users\\Felipe Mira\\Documents\\WordToPDF\\Template\\evidencia.png");
 		
 		Thread.sleep(5000);
 		
-		word.inserirEvidencia("Teste", false, true);
+		word.inserirEvidencia("Teste", true, true);
 		
 		word.finalizarEvidencia();
 	}
